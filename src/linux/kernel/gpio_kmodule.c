c4217346683cf87763b8d886e09706f824e182b9
Antonio Riccio <antonio.riccio.27@gmail.com>
2017-06-13 09:55:18 +0200

    Creazione cartelle per codice driver Linux e creazione scheletro doxygen per il kernel driver

---
+++ src/linux/kernel/gpio_kmodule.c
@@ -0,0 +1,25 @@
+/**
+* @file gpio_kmodule.c
+* @brief Implementazione del device driver per la periferica GPIO come modulo del Linux kernel.
+* @author: Antonio Riccio
+* @copyright
+* Copyright 2017 Antonio Riccio <antonio.riccio.27@gmail.com>, <antonio.riccio9@studenti.unina.it>.
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License as published by the
+* Free Software Foundation; either version 3 of the License, or any later version.
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+* details. You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*
+* @addtogroup KERNEL_MODULE
+* @{
+*/
+/***************************** Include Files ********************************/
+
+/**************************** Type Definitions ******************************/
+
+/************************** Function Prototypes *****************************/
+/** @} */
